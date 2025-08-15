21a47eaf9fd51f2c4f784cafc813ef7d0f246dd9
Marianne Azzopardi <marazzo94@gmail.com>
2025-08-15 18:18:25 +0100

    Add navigation bar and dashboard page

--- rc/App.jsx
+++ rc/App.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Dashboard from './pages/Dashboard';
+import NavBar from './components/NavBar';
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   min-height: 100vh;
-  align-items: center;
-  justify-content: center;
   background-color: #121212;
   color: #fff;
 `;
 
 function App() {
   return (
-    <Container>
-      <h1>Crypto Dashboard Starter</h1>
-    </Container>
+    <BrowserRouter>
+      <Container>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
 
