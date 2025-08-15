b7b4ce0b8e9ebfd2b19973ffca653eee28b0b6aa
Marianne Azzopardi <marazzo94@gmail.com>
2025-08-15 20:09:11 +0100

    refactor empty state styles

--- rc/components/EmptyState.jsx
+++ rc/components/EmptyState.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from './Button';
+import styles from './EmptyState.module.scss';
 
 function EmptyState({ message, action, actionLabel = 'Retry' }) {
   return (
-    <div style={{ textAlign: 'center', padding: '2rem' }}>
+    <div className={styles.wrapper}>
       <p>{message}</p>
       {action && (
-        <Button onClick={action} style={{ marginTop: '1rem' }}>
+        <Button onClick={action} className={styles.actionButton}>
           {actionLabel}
         </Button>
       )}
