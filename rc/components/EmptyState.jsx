c24b1246a97a021f4fb5f5e9ef613e24cdfee96b
Marianne Azzopardi <marazzo94@gmail.com>
2025-08-15 19:09:52 +0100

    style: add button styles

--- rc/components/EmptyState.jsx
+++ rc/components/EmptyState.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
 import Button from './Button';
-
-const Wrapper = styled.div`
-  text-align: center;
-  padding: 2rem;
-`;
-
-const ActionButton = styled(Button)`
-  margin-top: 1rem;
-`;
 
 function EmptyState({ message, action, actionLabel = 'Retry' }) {
   return (
-    <Wrapper>
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
       <p>{message}</p>
       {action && (
-        <ActionButton onClick={action}>{actionLabel}</ActionButton>
+        <Button onClick={action} style={{ marginTop: '1rem' }}>
+          {actionLabel}
+        </Button>
       )}
-    </Wrapper>
+    </div>
   );
 }
 
